af0fa775b08b13b6f37ba866a48cf25eb9278225
jaimeandc <50857746+jaimeandc@users.noreply.github.com>
2022-03-18 00:19:01 -0600

    Add files via upload

--- clientFTP.py
+++ clientFTP.py
@@ -6,8 +6,11 @@ import packet
 clientsock = ""
 newfile = ""
 packetsToSend = []
-windowsize = 0
+windowsize = 3
 expectedPacket = 0
+filename = ""
+windowbase = 1
+windowend = 3
 
 
 
@@ -23,31 +26,12 @@ def main():
     proto_type = input("What type of protocol would you like to use :")
     protopacket = packet.make(0, proto_type.encode())
     udt.send(protopacket, clientsock, addr)
-
-
-    try:
-        request = input("RTFCli> ")
-        cmd, filename = request.split(" ")
-        filename, filetype = filename.split(".")
-        newfile = filename + "." + filetype
-        filepacket = packet.make(0, newfile.encode())
-        udt.send(filepacket, clientsock,addr)
-        print("Sent")
-        #clientsock.send(packet.make(0,newfile))
-
-
-    except:
-        print("please enter cmd >[cmd] [filename.filetype]")
-        request = input("RTFCli> ")
-        newfile = filename + "." + filetype
-        #clientsock.send(newfile.encode())
-        #now implimenting with udt module
-        filepacket = packet.make(0, newfile.encode())
-        udt.send(filepacket, clientsock, addr)
-        print("Sent")
         #udt.send(packet.make(0,newfile.encode()),clientsock,addr)
         #ADD if to handle what type of protocol we are going to use.
+    if proto_type == "SNW":
         stop_n_wait()
+    elif proto_type == "GBN":
+        go_back_n(clientsock, addr)
 def stop_n_wait():
     while True:
         # Loop and wait for server to send messages back.
@@ -85,19 +69,57 @@ def stop_n_wait():
     clientSock.close()
 
 ##Begin with basic gbn to test connectivity. similar to what we had for our og code.
-def gbn():
+def go_back_n(conn,addr):
+    try:
+        request = input("RTFCli> ")
+        cmd, filename = request.split(" ")
+        filename, filetype = filename.split(".")
+        newfile = filename + "." + filetype
+        print(newfile)
+        filepacket = packet.make(0, newfile.encode())
+        udt.send(filepacket, conn ,addr)
+        print("Sent")
+        #clientsock.send(packet.make(0,newfile))
+
+
+    except:
+        print("please enter cmd >[cmd] [filename.filetype]")
+        request = input("RTFCli> ")
+        newfile = filename + "." + filetype
+        #clientsock.send(newfile.encode())
+        #now implimenting with udt module
+        filepacket = packet.make(0, newfile.encode())
+        udt.send(filepacket, conn, addr)
+    print(newfile)
     print("Hello welcome to GBN")
-    ##From this point on we begin with sequence number 0 and
-    currentSegNum = 1
-    f = open(newfile, "a")
-    #need to change loop to work with timer and resend after timeout
+    ##From this point on we wait sequence number 1 and get ready to send acks.
+    packetsRecv = []
+    expectinACK = 1
+    previousSentAck = " "
+    f = open(newfile, 'wb')
     while True:
         #data = clientsock.recv(1000).decode()
-        ack_packet, addr = udt.recv(clientsock)
+        #at this point we enter a loop waiting for the payload and sending retransmitions if needed.
+        ack_packet, addr = udt.recv(conn)
         ack_seq_num, data = packet.extract(ack_packet)
         if not data:
             f.close()
             break
+        # check if it is what we are expecting
+        if ack_seq_num == expectinACK:
+            packetsRecv[ack_seq_num] = [data]
+            f.write(data)
+            expectinACK = 1 + expectinACK
+            ACKpacket = packet.make(ack_seq_num + 1, packet.make_empty())
+            udt.send(ACKpacket, conn, addr)
+            #save a copy of the previous sent ack so that if we hit a timout we can resend the previous sent packet
+            previousSentAck = ACKpacket
+
+    else:
+        print("sending ack for previous packet")
+        udt.send(previousSentAck, conn, addr)
+
+
 
 
 
